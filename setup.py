f2ab715c913f664061e2e23e8a512cd9ba0bf7d4
er-eis <eeisenberg0@gmail.com>
2023-12-17 16:57:18 -0500

    no entry point

--- setup.py
+++ setup.py
@@ -19,9 +19,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
-    entry_points={
-        "console_scripts": [
-            "your_script=main",
-        ],
-    },
+    entry_points={},
 )
