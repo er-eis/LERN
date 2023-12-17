d8720cd8b9210f93b9b470b48dc3c412cf054da6
er-eis <eeisenberg0@gmail.com>
2023-12-17 00:17:59 -0500

    better logging

--- utils.py
+++ utils.py
@@ -2,9 +2,18 @@ import logging
 
 
 def setup_logging():
+    log_format = "%(asctime)s - %(levelname)s - [%(pathname)s] %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
     # Configure basic logging
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format=log_format,
+        datefmt=date_format,
     )
+
+    uvicorn_logger = logging.getLogger("uvicorn")
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(log_format, datefmt=date_format))
+    uvicorn_logger.handlers = [handler]
+    uvicorn_logger.setLevel(logging.INFO)
